c0081385cff506e53f4f9962c4d997b8ed9b98a1
mainulmbstu <mainul1945@gmail.com>
2024-09-16 00:08:13 +0600

    loading page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,15 @@
+const Loading = () => {
+  return (
+<div className=" h-100 d-flex justify-content-center">
+        <div
+          className="spinner-border align-self-center"
+          style={{ width: "5rem", height: "5rem" }}
+          role="status"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </div>
+</div>
+  );
+};
+
+export default Loading;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import { useSearch } from "../context/SearchContext";
 import Layout from "../components/Layout";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import Loading from "../components/Loading";
 
 const Home = () => {
   let { category, } = useStore();
@@ -106,21 +107,21 @@ const Home = () => {
   }, []);
 
   return (
-    <Layout title={'home'}>
-    <div className="row px-3">
-      <div className="col-md-2">
-        <h5>Category</h5>
-        <div className=" d-flex flex-column">
-          {category.map((item) => (
-            <Checkbox
-              key={item._id}
-              onChange={(e) => catHandle(e.target.checked, item._id)}
-            >
-              {item.name}
-            </Checkbox>
-          ))}
+    <Layout title={"home"}>
+      <div className="row px-md-4">
+        <div className="col-md-2">
+          <h5>Category</h5>
+          <div className=" d-flex flex-column">
+            {category.map((item) => (
+              <Checkbox
+                key={item._id}
+                onChange={(e) => catHandle(e.target.checked, item._id)}
+              >
+                {item.name}
+              </Checkbox>
+            ))}
 
-          {/* {category?.map((item) => {
+            {/* {category?.map((item) => {
             return (
               <div key={item._id} className="input-group mb-3">
                 <div className="input-group-text">
@@ -145,11 +146,11 @@ const Home = () => {
               </div>
             );
           })} */}
-        </div>
+          </div>
 
-        <div>
-          <h5>Price Category</h5>
-          {/* <Radio.Group onChange={(e) => setPriceCat(e.target.value)}>
+          <div>
+            <h5>Price Category</h5>
+            {/* <Radio.Group onChange={(e) => setPriceCat(e.target.value)}>
             {priceCategory.map((p) => {
               return (
                 <div key={p._id}>
@@ -160,78 +161,79 @@ const Home = () => {
               );
             })}
           </Radio.Group> */}
-          <div className=" d-flex flex-column">
-            <div>
-              <input
-                onChange={() => setPriceCat([1, 20])}
-                type="radio"
-                name="kkk"
-                id="one"
-              />
-              <label htmlFor="one">1-20</label>
-            </div>
-            <div>
-              <input
-                onChange={() => setPriceCat([21, 40])}
-                type="radio"
-                name="kkk"
-                id="two"
-              />
-              <label htmlFor="two">21-40</label>
-            </div>
-            <div>
-              <input
-                onChange={() => setPriceCat([41, 60])}
-                type="radio"
-                name="kkk"
-                id="three"
-              />
-              <label htmlFor="three">41-60</label>
+            <div className=" d-flex flex-column">
+              <div>
+                <input
+                  onChange={() => setPriceCat([1, 20])}
+                  type="radio"
+                  name="kkk"
+                  id="one"
+                />
+                <label htmlFor="one">1-20</label>
+              </div>
+              <div>
+                <input
+                  onChange={() => setPriceCat([21, 40])}
+                  type="radio"
+                  name="kkk"
+                  id="two"
+                />
+                <label htmlFor="two">21-40</label>
+              </div>
+              <div>
+                <input
+                  onChange={() => setPriceCat([41, 60])}
+                  type="radio"
+                  name="kkk"
+                  id="three"
+                />
+                <label htmlFor="three">41-60</label>
+              </div>
             </div>
           </div>
+          <div className="mt-3">
+            <button
+              onClick={() => window.location.reload()}
+              className=" btn btn-success"
+            >
+              Reset Filter
+            </button>
+          </div>
         </div>
-        <div className="mt-3">
-          <button
-            onClick={() => window.location.reload()}
-            className=" btn btn-success"
-          >
-            Reset Filter
-          </button>
-        </div>
-      </div>
 
-      <div className="col-md-10">
-        
-          <h3>{!checkedCat.length ? "All Products" : "Products by category"}</h3>
-          {
-            loadingHome && <h1> Loading...</h1>
-          }
+        <div className="col-md-10">
+          <h3>
+            {!checkedCat.length ? "All Products" : "Products by category"}
+          </h3>
+          {loadingHome && <Loading />}
           <h3 className=" text-danger">
-            
-          {!products?.length ? "No Product Found!!" : ""}
-        </h3>
-        <div className="row g-3">
-          {products?.map((item) => {
-            return (
-              <div key={item._id} className="col-md-3  ">
-                <div className="card h-100">
-                  <LazyLoadImage
-                    src={`${item?.picture?.secure_url}`}
-                    className=" "  height={150}
-                    alt="image"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{item.name}</h5>
-                    <div className="card-text">
-                      <p className="m-0">Category: {item?.category?.name} </p>
-                      <p className="m-0">Price: {item.price} </p>
-                      <p className="m-0">Available quantity: {item.quantity} </p>
-                      <p className="m-0">
-                        Description: {item.description.substring(0, 8)} ....{" "}
-                      </p>
+            {!products?.length ? "No Product Found!!" : ""}
+          </h3>
+          <div className="row g-3">
+            {products?.map((item) => {
+              return (
+                <div key={item._id} className="col-md-3  ">
+                  <div className="card h-100">
+                    <LazyLoadImage
+                      src={`${item?.picture?.secure_url}`}
+                      className=" "
+                      height={150}
+                      alt="image"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{item.name}</h5>
+                      <div className="card-text">
+                        <p className="m-0">Category: {item?.category?.name} </p>
+                        <p className="m-0">Price: {item.price} </p>
+                        <p className="m-0">
+                          Available quantity: {item.quantity}{" "}
+                        </p>
+                        <p className="m-0">
+                          Description: {item.description.substring(0, 8)} ....{" "}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  <div className=" d-flex justify-content-evenly">
+                    <div className=" d-flex justify-content-evenly">
                       <Link to={`products/more-info/${item?._id}`}>
                         <button
                           // onClick={() => navigate(`products/more-info/${item?._id}`)}
@@ -240,35 +242,39 @@ const Home = () => {
                           More info
                         </button>
                       </Link>
-                    <button
-                      onClick={() => {
-                        setCart([...cart, item])
-                        localStorage.setItem('cart', JSON.stringify([...cart, item]))
-                        toast.success(`${item.name} added to Cart`);
-                      }}
-                      className="btn btn-info mt-auto mb-1">
-                      Add to cart
-                    </button>
+                      <button
+                        onClick={() => {
+                          setCart([...cart, item]);
+                          localStorage.setItem(
+                            "cart",
+                            JSON.stringify([...cart, item])
+                          );
+                          toast.success(`${item.name} added to Cart`);
+                        }}
+                        className="btn btn-info mt-auto mb-1"
+                      >
+                        Add to cart
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <div className="m-3 d-flex justify-content-center">
-        {products && products.length < total && (
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              setPage(page + 1);
-            }}
-            className="btn btn-success px-5"
-          >
-            {loading ? "loading..." : "Load More"}
-          </button>
-        )}
-      </div>
+        <div className="m-3 d-flex justify-content-center">
+          {products && products.length < total && (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setPage(page + 1);
+              }}
+              className="btn btn-success px-5"
+            >
+              {loading ? "loading..." : "Load More"}
+            </button>
+          )}
+        </div>
       </div>
     </Layout>
   );
