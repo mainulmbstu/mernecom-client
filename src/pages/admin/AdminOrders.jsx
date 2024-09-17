e2aa2482571571f863ac66c7023fa2debb7cdb4f
mainulmbstu <mainul1945@gmail.com>
2024-09-17 12:01:02 +0600

    admin order limit

--- src/pages/admin/AdminOrders.jsx
+++ src/pages/admin/AdminOrders.jsx
@@ -4,8 +4,10 @@ import AdminMenu from "./AdminMenu";
 import moment from "moment";
 import { Select } from "antd";
 import Layout from "../../components/Layout";
+import axios from "axios";
 let { Option } = Select;
 import Loading from "./../../components/Loading";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const AdminOrders = () => {
   let [adminOrders, setAdminOrders] = useState([]);
@@ -18,24 +20,36 @@ const AdminOrders = () => {
   ]);
   let [loading, setLoading] = useState(false);
   let { token, userInfo } = useAuth();
+  //============================================================
+  let [page, setPage] = useState(1);
+  let [total, setTotal] = useState(0);
 
-    let getAdminOrders = async () => {
-      try {
-        setLoading(true);
-         let res= await fetch(`${import.meta.env.VITE_BASE_URL}/admin/order-list`, {
-            method: "GET",
-            headers: { Authorization: `Bearer ${token}` },
-          })
-          let data = await res.json()
-          setAdminOrders(data.orderList);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  let getAdminOrders = async () => {
+    try {
+      setLoading(true);
+      let { data } = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/admin/order-list`,
+        {
+          params: {
+            page: page,
+            size: 8,
+          },
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      // let data = await res.json()
+      setPage(page+1);
+      setTotal(data.total)
+      setAdminOrders([...adminOrders,...data.orderList]);
+      console.log(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-   if (token && userInfo.role) getAdminOrders();
+    if (token && userInfo.role) getAdminOrders();
   }, [token, userInfo.role]);
 
   //   let deletefn = async (id) => {
@@ -71,100 +85,112 @@ const AdminOrders = () => {
     }
   };
 
-  let totalPrice= adminOrders?.length && adminOrders?.reduce((previous, current) => {
-    return previous + current.total
-  }, 0);
-//===============================================================
+  let totalPrice =
+    adminOrders?.length &&
+    adminOrders?.reduce((previous, current) => {
+      return previous + current.total;
+    }, 0);
+  //===============================================================
   return (
     <Layout title={"Admin orders"}>
       <div className="row ">
+        {/* <h1>{[...Array(adminOrders.length/2)].map((_, i) => {
+          return <span>{i+1} </span>
+        })}</h1> */}
         <div className="col-md-3 p-2">
           <div className="card p-2  sticky-top z-0">
             <AdminMenu />
+            <div className=" card p-2 mt-5">
+              <h4>Total Orders: ({adminOrders?.length} of {total}) </h4>
+              <h4> Total Sale: BDT {totalPrice} </h4>
+            </div>
           </div>
         </div>
-        <div className=" col-sm-9 p-2">
-          <div className=" card p-2 text-center">
-            <h2>Total Orders: ({adminOrders?.length}) </h2>
-            <h2> Total Sale: BDT {totalPrice} </h2>
-          </div>
-
+        <div className=" col-md-9 px-2">
           <div className="row ">
-            {loading && <Loading />}
+           
 
-            {adminOrders.length &&
-              adminOrders?.map((item, i) => {
-                return (
-                  <div key={item._id} className=" mt-5 shadow">
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th scope="col">#</th>
-                          <th scope="col">Status</th>
-                          <th scope="col">User-email</th>
-                          <th scope="col">User-Address</th>
-                          <th scope="col">Payment</th>
-                          <th scope="col">Qnty</th>
-                          <th scope="col">Total Price</th>
-                          <th scope="col">Time</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>{i + 1} </td>
-                          <td>
-                            <Select
-                              variant={false}
-                              defaultValue={item?.status}
-                              onChange={(val) => statusHandle(item._id, val)}
-                            >
-                              {status.map((st, i) => (
-                                <Option key={i} value={st}>
-                                  {st}{" "}
-                                </Option>
-                              ))}
-                            </Select>
-                          </td>
+            <InfiniteScroll
+              dataLength={adminOrders.length}
+              next={getAdminOrders}
+              hasMore={adminOrders.length < total}
+              loader={<h1>Loading...</h1>}
+              endMessage={<h4 className=" text-center">All items loaded</h4>}
+            >
+              {adminOrders.length &&
+                adminOrders?.map((item, i) => {
+                  return (
+                    <div key={item._id} className=" mt-2 p-1 shadow">
+                      <table className="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">User-email</th>
+                            <th scope="col">User-Address</th>
+                            <th scope="col">Payment</th>
+                            <th scope="col">Qnty</th>
+                            <th scope="col">Total Price</th>
+                            <th scope="col">Time</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>{i + 1} </td>
+                            <td>
+                              <Select
+                                variant={false}
+                                defaultValue={item?.status}
+                                onChange={(val) => statusHandle(item._id, val)}
+                              >
+                                {status.map((st, i) => (
+                                  <Option key={i} value={st}>
+                                    {st}{" "}
+                                  </Option>
+                                ))}
+                              </Select>
+                            </td>
 
-                          <td>{item?.user?.email} </td>
-                          <td>{item?.user?.address} </td>
-                          <td>
-                            {item?.payment?.status ? "Success" : "Failed"}{" "}
-                          </td>
-                          <td>{item?.products?.length} </td>
-                          <td>{item?.total} </td>
-                          <td>{moment(item?.createdAt).fromNow()} </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                    {item?.products?.length &&
-                      item?.products?.map((p, i) => {
-                        return (
-                          <div key={i} className="row g-5">
-                            <div className="row g-4">
-                              <div className=" col-4">
-                                <img
-                                  src={`${p?.picture?.secure_url}`}
-                                  className=" ms-3"
-                                  width={100}
-                                  height={100}
-                                  alt="image"
-                                />
-                              </div>
-                              <div className=" col-8 d-flex flex-column">
-                                <div>
-                                  <h5>Name: {p?.name} </h5>
-                                  <p>Category: {p?.category?.name} </p>
-                                  <p>Price: {p?.price} </p>
+                            <td>{item?.user?.email} </td>
+                            <td>{item?.user?.address} </td>
+                            <td>
+                              {item?.payment?.status ? "Success" : "Failed"}{" "}
+                            </td>
+                            <td>{item?.products?.length} </td>
+                            <td>{item?.total} </td>
+                            <td>{moment(item?.createdAt).fromNow()} </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      {item?.products?.length &&
+                        item?.products?.map((p, i) => {
+                          return (
+                            <div key={i} className="row g-5">
+                              <div className="row g-4">
+                                <div className=" col-4">
+                                  <img
+                                    src={`${p?.picture?.secure_url}`}
+                                    className=" ms-3"
+                                    width={100}
+                                    height={100}
+                                    alt="image"
+                                  />
+                                </div>
+                                <div className=" col-8 d-flex flex-column">
+                                  <div>
+                                    <h5>Name: {p?.name} </h5>
+                                    <p>Category: {p?.category?.name} </p>
+                                    <p>Price: {p?.price} </p>
+                                  </div>
                                 </div>
                               </div>
                             </div>
-                          </div>
-                        );
-                      })}
-                  </div>
-                );
-              })}
+                          );
+                        })}
+                    </div>
+                  );
+                })}
+            </InfiniteScroll>
           </div>
         </div>
       </div>
