a21a13acd604d42f2296b9cd402b3978ab5c1629
mainulmbstu <mainul1945@gmail.com>
2024-10-05 09:08:40 +0600

    mmm

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -44,92 +44,93 @@ let [page, setPage] = useState(1);
 
   return (
     <Layout title={`Category-${params.slug}`}>
-      <div>
-        <div className="px-2">
-          <h3 className=" text-capitalize">
-            Category: {params.slug}({products?.length} of {total} )
-          </h3>
-          <h3 className=" text-danger">
-            {!products?.length ? "No Product Found!!" : ""}
-          </h3>
-            <InfiniteScroll
-              dataLength={products.length}
-              next={() => {
-                setPage(page + 1);
-                getProducts(page + 1);
-              }}
-              hasMore={products.length < total}
-              loader={<h1>Loading...</h1>}
-              endMessage={<h4 className=" text-center">All items loaded</h4>}
-          >
-            {loading && <Loading/>}
-          <div className="row g-3">
-              {products?.length &&
-                products?.map((item) => {
-                  return (
-                    <div key={item._id} className="col-md-3  ">
-                      <div className="card h-100">
-                        <img
-                          src={`${item?.picture?.secure_url}`}
-                          className=" card-img-top"
-                          height={200}
-                          alt="image"
-                        />
-                        <div className="card-body">
-                          <h5 className="card-title">{item.name}</h5>
-                          <div className="card-text">
-                            <p className="m-0">
-                              Category: {item.category.name}{" "}
-                            </p>
-                            <p className="m-0">Price: {item.price} </p>
-                            <p className="m-0">
-                              Available quantity: {item.quantity}{" "}
-                            </p>
-                            <p className="m-0">
-                              Description: {item.description.substring(0, 8)}{" "}
-                              ...
-                            </p>
+     <div className={loading && 'dim'}>
+        <div>
+          <div className="px-2">
+            <h3 className=" text-capitalize">
+              Category: {params.slug}({products?.length} of {total} )
+            </h3>
+            <h3 className=" text-danger">
+              {!products?.length ? "No Product Found!!" : ""}
+            </h3>
+              <InfiniteScroll
+                dataLength={products.length}
+                next={() => {
+                  setPage(page + 1);
+                  getProducts(page + 1);
+                }}
+                hasMore={products.length < total}
+                loader={<h1>Loading...</h1>}
+                endMessage={<h4 className=" text-center">All items loaded</h4>}
+            >
+            <div className="row g-3">
+                {products?.length &&
+                  products?.map((item) => {
+                    return (
+                      <div key={item._id} className="col-md-3  ">
+                        <div className="card h-100">
+                          <img
+                            src={`${item?.picture[0]?.secure_url}`}
+                            className=" card-img-top"
+                            height={200}
+                            alt="image"
+                          />
+                          <div className="card-body">
+                            <h5 className="card-title">{item.name}</h5>
+                            <div className="card-text">
+                              <p className="m-0">
+                                Category: {item.category.name}{" "}
+                              </p>
+                              <p className="m-0">Price: {item.price} </p>
+                              <p className="m-0">
+                                Available quantity: {item.quantity}{" "}
+                              </p>
+                              <p className="m-0">
+                                Description: {item.description.substring(0, 8)}{" "}
+                                ...
+                              </p>
+                            </div>
+                          </div>
+                          <div className=" d-flex justify-content-evenly">
+                            <Link to={`/products/more-info/${item._id}`}>
+                              <button
+                                // onClick={() => navigate(`products/more-info/${item?._id}`)}
+                                className="btn btn-primary "
+                              >
+                                More info
+                              </button>
+                            </Link>
+                            <button className="btn btn-info mt-auto mb-1">
+                              Add to cart
+                            </button>
                           </div>
                         </div>
-                        <div className=" d-flex justify-content-evenly">
-                          <Link to={`/products/more-info/${item._id}`}>
-                            <button
-                              // onClick={() => navigate(`products/more-info/${item?._id}`)}
-                              className="btn btn-primary "
-                            >
-                              More info
-                            </button>
-                          </Link>
-                          <button className="btn btn-info mt-auto mb-1">
-                            Add to cart
-                          </button>
-                        </div>
                       </div>
-                    </div>
-                  );
-                })}
-          </div>
-            </InfiniteScroll>
-          <div className="d-flex">
-            {products.length < total ? (
-              <>
-                <button
-                  onClick={() => {
-                    setPage(page + 1);
-                    getProducts(page + 1);
-                  }}
-                  className="btn btn-primary my-3 px-3 mx-auto"
-                  disabled={loading}
-                >
-                  {loading ? "Loading..." : "Load More"}
-                </button>
-              </>
-            ) : (
-              ""
-            )}
+                    );
+                  })}
+            </div>
+              </InfiniteScroll>
+            <div className="d-flex">
+              {products.length < total ? (
+                <>
+                  <button
+                    onClick={() => {
+                      setPage(page + 1);
+                      getProducts(page + 1);
+                    }}
+                    className="btn btn-primary my-3 px-3 mx-auto"
+                    disabled={loading}
+                  >
+                    {loading ? "Loading..." : "Load More"}
+                  </button>
+                </>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
         </div>
-      </div>
+     </div>
     </Layout>
   );
 }
--- src/pages/user/Gallery.jsx
+++ src/pages/user/Gallery.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Layout from "../../components/Layout";
-import Loading from "../../components/Loading";
 import { useAuth } from "../../context/AuthContext";
 import { toast } from "react-toastify";
 
@@ -81,77 +80,78 @@ const Gallery = () => {
 
   return (
     <Layout title="Gallery">
-      {loading && <Loading />}
-      {userInfo?.role && (
-        <div className="px-2 my-4">
-          <form onSubmit={submitHandle}>
-            <label htmlFor="pic" className="">
-              Upload gallery image (jpeg, jpg, png, webp, Max size- 4mb)
-            </label>
-            <input
-              className=" form-control mb-2"
-              id="pic"
-              type="file"
-              name="picture"
-              accept="image/*"
-              multiple
-              onChange={(e) => {
-                setInputVal([...e.target.files]);
-              }}
-            />
-            <div className="">
-              {loading ? (
-                <>
-                  <button
-                    className="btn btn-primary fs-5"
-                    type="button"
-                    disabled
-                  >
-                    <span
-                      className="spinner-grow spinner-grow-sm"
-                      role="status"
-                      aria-hidden="true"
-                    ></span>
-                    Uploadin data...
-                  </button>
-                </>
-              ) : (
-                <>
-                  <button
-                    className=" btn  btn-primary text-white fs-5 btn-outline-dark"
-                    type="submit"
-                  >
-                    Upload Image
-                  </button>
-                </>
-              )}
-            </div>
-          </form>
-        </div>
-      )}
-      <InfiniteScroll
-        dataLength={gallery?.length}
-        next={getGallery}
-        hasMore={gallery[0]?.picture?.length < total}
-        loader={<h1>Loading...</h1>}
-        endMessage={<h4 className=" text-center">All items loaded</h4>}
-      >
-        <div className="row mx-2">
-          {gallery.length &&
-            gallery[0]?.picture?.reverse().map((item, index) => {
-              return (
-                <div key={index} className="col-md-3 border text-center p-2">
-                  <LazyLoadImage
-                    src={`${import.meta.env.VITE_BASE_URL}/${item}`}
-                    alt="image"
-                    width={"100%"}
-                    height={300}
-                  />
-                </div>
-              );
-            })}
-        </div>
-      </InfiniteScroll>
+     <div className={loading && 'dim'}>
+        {userInfo?.role && (
+          <div className="px-2 my-4">
+            <form onSubmit={submitHandle}>
+              <label htmlFor="pic" className="">
+                Upload gallery image (jpeg, jpg, png, webp, Max size- 4mb)
+              </label>
+              <input
+                className=" form-control mb-2"
+                id="pic"
+                type="file"
+                name="picture"
+                accept="image/*"
+                multiple
+                onChange={(e) => {
+                  setInputVal([...e.target.files]);
+                }}
+              />
+              <div className="">
+                {loading ? (
+                  <>
+                    <button
+                      className="btn btn-primary fs-5"
+                      type="button"
+                      disabled
+                    >
+                      <span
+                        className="spinner-grow spinner-grow-sm"
+                        role="status"
+                        aria-hidden="true"
+                      ></span>
+                      Uploadin data...
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button
+                      className=" btn  btn-primary text-white fs-5 btn-outline-dark"
+                      type="submit"
+                    >
+                      Upload Image
+                    </button>
+                  </>
+                )}
+              </div>
+            </form>
+          </div>
+        )}
+        <InfiniteScroll
+          dataLength={gallery?.length}
+          next={getGallery}
+          hasMore={gallery[0]?.picture?.length < total}
+          loader={<h1>Loading...</h1>}
+          endMessage={<h4 className=" text-center">All items loaded</h4>}
+        >
+          <div className="row mx-2">
+            {gallery.length &&
+              gallery[0]?.picture?.reverse().map((item, index) => {
+                return (
+                  <div key={index} className="col-md-3 border text-center p-2">
+                    <LazyLoadImage
+                      src={`${import.meta.env.VITE_BASE_URL}/${item}`}
+                      alt="image"
+                      width={"100%"}
+                      height={300}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+        </InfiniteScroll>
+     </div>
     </Layout>
   );
 };
